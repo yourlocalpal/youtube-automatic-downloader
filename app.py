5f6f50fa895a61a326727f0a065953f5365bafa5
yourlocalpal <118146578+yourlocalpal@users.noreply.github.com>
2023-03-29 23:38:49 -0500

    ffmpeg functionality
    !!! If you are running Windows then you must do this !!!
    
    To add FFmpeg to your system path in Windows, you can follow these steps:
    
        Download FFmpeg from the official website (https://ffmpeg.org/download.html#build-windows) and extract the contents to a folder (e.g., C:\ffmpeg).
        Right-click on "This PC" (or "My Computer") and select "Properties".
        Click on "Advanced system settings" and then click on the "Environment Variables" button.
        In the "System variables" section, scroll down and find the "Path" variable and click on "Edit".
        Click on "New" and enter the path to the FFmpeg folder (e.g., C:\ffmpeg\bin) and click "OK".
        Click "OK" to close all windows and restart your command prompt.
    
    After following these steps, you should be able to call FFmpeg from the command prompt by simply typing "ffmpeg".
    
    or simply: scoop install ffmpeg
    (you need scoop)

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ import os
 import datetime
 import time
 import yt_dlp
+import ffmpeg
 
 # Define the path where videos will be downloaded
 download_path = "G:\\plex\\Twitch" # This Path is for Windows OS. Update for your OS and path.
 
 # Define the path to the file that stores the download history
-history_file = f"{download_path}\\downloaded.txt"
+history_file = os.path.join(download_path, "downloaded.txt")
 
 # Create an empty set to store the IDs of downloaded videos
 downloaded_ids = set()
@@ -27,55 +28,76 @@ while True:
     past_time = current_time - datetime.timedelta(hours=48)
 
     # Convert times to string format
-    current_time_str = current_time.strftime("%Y%m%d%H%M%S")
-    past_time_str = past_time.strftime("%Y%m%d%H%M%S")
-
-    # Define the output template for downloaded videos
-    output_template = f"{download_path}\\%(uploader)s\\%(title)s_{current_time_str}.%(ext)s"
+    current_time_str = current_time.strftime("%B %d %Y")
+    past_time_str = past_time.strftime("%B %d %Y")
 
     # Define the options for yt-dlp
     ydl_opts = {
         'ignoreerrors': True,
         'nopart': True,
         'download_archive': history_file,
-        'outtmpl': output_template,
         'dateafter': past_time_str,
         'datebefore': current_time_str,
         'writedescription': True,
         'writeinfojson': True,
         'writethumbnail': True,
-        'playlistend': 10,
-        'page': 1
+        'playlistend': 3,
+        'page': 1,
+        'outtmpl': 'G:/plex/Twitch/%(title)s.%(ext)s',
+        'format': 'bestvideo+bestaudio/best',
     }
 
     # Create a new yt-dlp object with the specified options
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+
         # Get the metadata for the videos from the specified channel URL
-        print(f"Checking for new videos uploaded in the past 24 hours...")
+        print(f"Checking for new videos uploaded in the past 48 hours...")
         metadata = ydl.extract_info('https://www.youtube.com/@AsmonTV/videos', download=True)
+
+        # Wait for one hour before checking for new videos again
+        print("Waiting for one hour before checking for new videos again...")
+        time.sleep(3600)
 
         # Iterate over the metadata for each video
         for video in metadata['entries']:
-            # Check if the video was uploaded within the last 24 hours
+            #ydl.process_ie_result(video, download=True)
+            # Check if the video was uploaded within the last 48 hours
             if datetime.datetime.strptime(video['upload_date'], "%Y%m%d") >= past_time:
                 # Check if the video has already been downloaded
                 if video['id'] not in downloaded_ids:
                     # Download the video
                     video_date = datetime.datetime.strptime(video['upload_date'], "%Y%m%d").strftime("%Y-%m-%d")
                     output_filename = f"{video_date} {video['title']}.{video['ext']}"
-                    output_template = os.path.join(download_path, "%(uploader)s", output_filename)
-                    ydl_opts['outtmpl'] = output_template
+                    output_path = os.path.join(download_path, video['uploader'], output_filename)
                     print(f"Downloading video '{video['title']}'...")
-                    ydl.download(['https://www.youtube.com/watch?v=' + video['id']])
+                    ydl_opts['outtmpl'] = output_path
+                    ydl.process_ie_result(video, download=True)
 
-                    # Add the video's ID to the set of downloaded IDs
+                    # Merge the video and audio streams using ffmpeg
+                    video_path = os.path.join(download_path, video['uploader'], f"{video_date} {video['title']}.mp4")
+                    audio_path = os.path.join(download_path, video['uploader'], f"{video_date} {video['title']}.m4a")
+                    output_path = os.path.join(download_path, video['uploader'], output_filename)
+                    video_stream = ffmpeg.input(video_path)
+                    audio_stream = ffmpeg.input(audio_path)
+                    merged_stream = ffmpeg.concat(video_stream, audio_stream, v=1, a=1).output(output_path,
+                                                                                               codec="copy").run()
+                    print(f"Video '{video['title']}' downloaded and merged successfully!")
+
+                    # Add the downloaded ID to the history file
                     downloaded_ids.add(video['id'])
+                    with open(history_file, "a") as f:
+                        f.write(f"{video['id']}\n")
 
-    # Write the set of downloaded IDs to the history file
-    with open(history_file, "w") as f:
-        for id in downloaded_ids:
-            f.write(f"ID: {id}\n")
+                    # Delete the original video and audio files
+                    os.remove(video_path)
+                    os.remove(audio_path)
+                else:
+                    print(f"Video '{video['title']}' already downloaded, skipping...")
+            else:
+                print(f"Video '{video['title']}' was not uploaded within the last 48 hours, skipping...")
 
-    # Wait for one hour before checking for new videos again
-    print("Waiting for one hour before checking for new videos again...")
-    time.sleep(3600)
+                # Print a summary of the downloaded videos
+            print(f"\n{len(downloaded_ids)} videos downloaded in total.")
+
+            if __name__ == "__main__":
+                main()
